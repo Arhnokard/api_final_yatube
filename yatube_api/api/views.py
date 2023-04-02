ac6c83a1a7a6091f338855b4e6e60f7c1732abf6
Евгений Поздняков <Arhnokard@gmail.com>
2023-04-02 11:59:48 +0300

    API_FINAL_YATUBE complet v2.2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,11 +4,9 @@ from rest_framework import filters
 from rest_framework import pagination
 from rest_framework import permissions
 from rest_framework import mixins
-from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.response import Response
 
-from posts.models import Post, Group, Follow
+from posts.models import Post, Group
 from .permissions import AuthorEditOrReadOnly
 from .serializers import (PostSerializer, GroupSerializer,
                           CommentSerializer, FollowSerializer)
